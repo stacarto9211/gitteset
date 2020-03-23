585f70d9868c890fc2237fda40e11619fd8bad96
stacarto <dmlska321@hotmail.com>
2020-03-23 16:45:49 +0900

    개좆같다~!~!

---
+++ gittest/src/gittest/HotfixTest2.java
@@ -0,0 +1,11 @@
+package gittest;
+
+public class HotfixTest2 {
+
+	public static void main(String[] args) {
+		//프로젝트명->오른쪽->Remote->Switch->New Branch->hotfix
+		//hotfix브랜치에서 저장ㅇ점을 한개더 가지기 위한 소스추가
+		System.out.println("Hotfix 브랜치에서 2번째로 추가한 소스임.");
+	}
+
+}
--- gittest/src/gittest/asdf.java
+++ gittest/src/gittest/asdf.java
@@ -3,6 +3,6 @@ package gittest;
 public class asdf {
 
 	public static void main(String[] grgs) {
-		System.out.println("엿머거라 깃허브");
+		System.out.println("엿머거라 깃허브 개좆같네");
 	}
 }
